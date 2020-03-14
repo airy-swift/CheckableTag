71feaac5a2e97068ccbc0b7235085e335db3d2d9
airy-swift <v66@tapi.re>
2020-03-14 12:18:58 +0900

    just add and make extensions
    必要になりそうなエクステンションを追加。

---
+++ CheckableTag/CheckableTagMain/ExtensionCollectionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionCollectionDelegate.swift
+//  CheckableTag
+//
+//  Created by airy on 2020/03/14.
+//  Copyright © 2020 airy. All rights reserved.
+//
+
+import UIKit
+
+extension CheckableTag: UICollectionViewDelegate {
+    
+}
---
+++ CheckableTag/CheckableTagMain/ExtensionFlowLayoutDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionFlowLayoutDelegate.swift
+//  CheckableTag
+//
+//  Created by airy on 2020/03/14.
+//  Copyright © 2020 airy. All rights reserved.
+//
+
+import UIKit
+
+extension CheckableTag: UICollectionViewDelegateFlowLayout {
+    
+}
