126c3436a5f6ed46033a5fe77a0ed098764b1647
airy-swift <v66@tapi.re>
2020-03-14 12:00:12 +0900

    init
    初期化

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by airy on 2020/03/14.
+//  Copyright © 2020 airy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
