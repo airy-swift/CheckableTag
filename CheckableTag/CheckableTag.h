67da2becd47e04ef41869ee32f3a2587b0e4a3c5
airy-swift <v66@tapi.re>
2020-03-14 11:57:24 +0900

    Initial Commit

---
+++ CheckableTag/CheckableTag.h
@@ -0,0 +1,19 @@
+//
+//  CheckableTag.h
+//  CheckableTag
+//
+//  Created by airy on 2020/03/14.
+//  Copyright © 2020 airy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckableTag.
+FOUNDATION_EXPORT double CheckableTagVersionNumber;
+
+//! Project version string for CheckableTag.
+FOUNDATION_EXPORT const unsigned char CheckableTagVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckableTag/PublicHeader.h>
+
+
