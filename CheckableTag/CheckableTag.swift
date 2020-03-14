e728d6ff4395702e187e4c1463d12250e338622a
airy-swift <v66@tapi.re>
2020-03-14 12:10:31 +0900

    just make class
    準備としてクラスを作成

---
+++ CheckableTag/CheckableTag.swift
@@ -0,0 +1,13 @@
+//
+//  CheckableTag.swift
+//  CheckableTag
+//
+//  Created by airy on 2020/03/14.
+//  Copyright © 2020 airy. All rights reserved.
+//
+
+import UIKit
+
+open class CheckableTag: UICollectionView {
+    
+}
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CheckableTag
 
 class ViewController: UIViewController {
 
